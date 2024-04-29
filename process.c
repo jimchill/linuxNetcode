a0ee84786f6b06d18f12d73415465a2e0eb916a9
jimchall <984359211@qq.com>
2024-04-29 19:48:22 +0800

    version 0.3.0

--- process.c
+++ process.c
@@ -9,14 +9,16 @@ void process_client(int s)
     char exitword[] = "quit";
     while (1)
     {
-        printf("waiting \n");  
-        send(s,exitword,strlen(exitword) + 1,0);
-        printf("client send!\n");
+        size  = read(0,buffer,1024);
+        if (size == 0)
+        {
+            break;
+        }
+        send(s,buffer,strlen(buffer)+1,0);
         size = recv(s,buffer,1024,0);
-        printf("server say:%s",buffer);
-        sleep(4);
+        printf("server:%s",buffer);
+        memset(buffer,'\0',1024);
     }
-
 }
 
 void process_server(int s)
@@ -27,14 +29,12 @@ void process_server(int s)
     while (1)
     {
         size = recv(s,buffer,1024,0);
-        printf("server has recv:%s!!\n",buffer);
+        printf("server has recv:%s\n",buffer);
         if (size == 0)
         {
             printf("server error!!!\n");
             return ;
         }
-    
-        sprintf(buffer,"%d bytes altogether\n",size);
         send(s,buffer,strlen(buffer)+1,0);
     }
 }
